f443be4c7a45328da4c118c80ca57cf3549f512c
tomgrek <tom.grek@gmail.com>
2024-01-11 21:43:23 -0800

    Getting there

---
+++ src/pyphonic/__init__.py
@@ -0,0 +1,192 @@
+# Status: Python + Python-in-c++ code is identical! Except in Python they have to `from pyphonic import state`
+
+# TODO do the flippin kubernetes thing
+# Destruction test!
+# Some kind of authentication on top of the magic packet, maybe just on initial connect
+# Add top menu bar to quickly try out some presets, ideally loaded from git... saturator, "piano minus the note's fundamental frequency as a
+# sine wave", etc.
+# Set up git repo, cicd, pypi for pyphonic. (Working on it)
+# Installer, packaging Python with it :(
+# Does it work on mac :(
+# Does it work in the DAW
+
+# python -c "import pyphonic; from pyphonic.demo import process;  pyphonic.start(process, 8019)"
+
+import socket
+import threading
+import math
+import struct
+import sys
+import time
+
+import mido
+
+all_threads = []
+should_stop = threading.Event()
+safe_to_transmit = threading.Event()
+
+in_buffer, out_buffer = [], []
+seq_num = 0
+block_size = 44100, 441
+
+class State:
+    sample_rate = 44100
+    block_size = 441
+    num_channels = 2
+    bpm = 120
+    sample_num = 0
+
+state = State()
+
+def transmit(socket, b: bytes):
+    magic_num = 15
+    header = magic_num.to_bytes(4, byteorder="little")
+    length = len(b)
+    length = length.to_bytes(4, byteorder="little")
+    msg = header + length + b
+    socket.sendall(msg)
+
+def handle(socket_, addr):
+    global cache
+    global cacheidx
+    global block_size
+
+    next_recv_size = None
+    empty_receives = 0
+    while not should_stop.is_set():
+        try:
+            data = bytearray(socket_.recv(next_recv_size or 1000).strip())
+        except socket.timeout:
+            should_stop.set()
+            continue
+        if not len(data):
+            empty_receives += 1
+            if empty_receives > 100:
+                print("Other end disconnected.")
+                should_stop.set()
+            continue
+
+        if b'EHLO' in data:
+            transmit(socket_, b'ok\n')
+            print(f"Received handshake from client.")
+            safe_to_transmit.set()
+        elif b'AUDIO' in data:
+            global in_buffer
+            global seq_num
+            desired_length = 100000
+            
+            if not next_recv_size:
+                while True:
+                    chunk = socket_.recv(2000).strip()
+                    if not chunk:
+                        break
+                    data.extend(chunk)
+                    if len(data) >= desired_length:
+                        break
+                    split_at = b'AUDIO'
+                    headers, content = data.split(split_at)
+                    hex_length = content[:4]
+
+                    content_length = int.from_bytes(hex_length, byteorder="little")
+                    (state.sample_rate, state.block_size, state.num_channels,
+                        state.bpm, state.sample_num) = struct.unpack('<2iBfl', content[4:21])
+                    print(state.sample_rate, state.block_size, state.num_channels, state.bpm, state.sample_num)
+                    
+                    desired_length = int.from_bytes(headers[-4:], byteorder='little') + len(headers)
+                    next_recv_size = int.from_bytes(headers[-4:], byteorder='little') + len(headers)
+                    print(f"Next recv size: {next_recv_size}")
+                    if len(data) >= desired_length:
+                        break
+            # magic_num:total_msg_length:AUDIO:(content+midi)_length:samplerate:blocksize:numChannels:bpm:sample_num
+            
+            (state.sample_rate, state.block_size, state.num_channels,
+                state.bpm, state.sample_num) = struct.unpack('<2iBfl', data[17:34])
+            
+            content = data[len(headers)+len(split_at)+4+4+4+1+4+4:]
+            midi, audio = content[:100], content[100:]
+            
+            assert len(audio) == content_length - 100
+            in_buffer.append((seq_num, audio, midi))
+            
+            seq_num += 1
+
+
+def shuffler(process_fn):
+
+    def wrapped_process_fn(midi_messages, audio):
+        return process_fn(midi_messages, audio)
+
+    while not should_stop.wait(0.0001):
+        while len(in_buffer) and not should_stop.is_set():
+            seq_num, audio_in, midi_in = in_buffer.pop(0)
+            audio_in = struct.unpack(f"{state.block_size*state.num_channels}f", audio_in) # bytes -> floats
+            p = mido.Parser()
+            p.feed(midi_in)
+            rendered_audio = wrapped_process_fn(p.messages, audio_in)
+            rendered_audio = struct.pack(f"{state.block_size*state.num_channels}f", *rendered_audio)
+
+            out_buffer.append((seq_num, rendered_audio))
+
+def responder(socket_):
+    safe_to_transmit.wait(10)
+    if not safe_to_transmit.is_set():
+        print("Handshake didn't happen")
+        sys.exit(1)
+    while not should_stop.wait(0.0001):
+        try:
+            if not len(out_buffer):
+                continue
+            s, o = out_buffer.pop(0)
+            transmit(socket_, o)
+        except BrokenPipeError:
+            print("Client disconnected, probably")
+            should_stop.set()
+            raise
+
+def start(process_fn, port=8015):
+    global in_buffer, out_buffer, seq_num
+    while True:
+        s = socket.socket()
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.settimeout(5)
+        s.bind(('0.0.0.0', port))
+        s.listen(1)
+        all_threads = []
+        safe_to_transmit.clear()
+        in_buffer, out_buffer = [], []
+        seq_num = 0
+        should_stop.clear()
+        shuffle_thread = threading.Thread(target=shuffler, args=(process_fn, ))
+        shuffle_thread.start()
+        all_threads.append(shuffle_thread)
+        try:
+            print(f"Listening on port {port}...")
+            conn = None
+            while not should_stop.is_set():
+                try:
+                    conn, addr = s.accept()
+                except socket.timeout:
+                    continue
+                conn.settimeout(1)
+                print(conn, addr)
+                handler_thread = threading.Thread(target=handle, args=(conn, addr))
+                handler_thread.start()
+                all_threads.append(handler_thread)
+                responder_thread = threading.Thread(target=responder, args=(conn, ))
+                responder_thread.start()
+                all_threads.append(responder_thread)
+        except KeyboardInterrupt:
+            print("User initiated stop.")
+            should_stop.set()
+        except BrokenPipeError:
+            print("Other end disconnected.")
+            should_stop.set()
+        finally:
+            print("Stopping listener...")
+            if s:
+                s.close()
+            for t in all_threads:
+                t.join()
+            print("Restarting listener in 1s. Ctrl-C to quit.")
+            time.sleep(1)
+        
---
+++ src/pyphonic/demo.py
@@ -0,0 +1,126 @@
+import math
+from pyphonic import state
+
+class Synth:
+    def __init__(self, sample_rate=44100):
+        self.angleDelta = 0.0
+        self.currentAngle = 0.0
+        self.tail = None
+        self.stopped = True
+        self.sample_rate = sample_rate # default, will be set by vst
+
+    def start_note(self, note, vel):
+        self.currentAngle = 0.0
+        self.level = vel * 0.05
+        self.tail = None
+        self.stopped = False
+
+        def noteToFreq(midi_note):
+            a = 440
+            return (a / 32) * (2 ** ((midi_note - 9) / 12))
+        self.cyclesPerSecond = noteToFreq(note)
+        self.cyclesPerSample = self.cyclesPerSecond / self.sample_rate
+        self.angleDelta = self.cyclesPerSample * 2 * math.pi
+    
+    def stop_note(self):
+        self.tail = 50000
+    
+    def renew(self):
+        self.tail = None
+    
+    def is_active(self):
+        if self.stopped:
+            return False
+        return True
+    
+    def render(self, num):
+        is_stopping = self.tail is not None
+        buf = []
+        if abs(self.angleDelta) != 0 and not self.stopped:
+            while num > 0:
+                num -= 1
+                cur = math.sin(self.currentAngle) * self.level
+                if self.tail is not None:
+                    cur *= self.tail / 50000
+                    self.tail = self.tail - 1
+                buf.append(cur)
+                self.currentAngle += self.angleDelta
+        else:
+            buf = [0.0 for _ in range(num)]
+        
+        if is_stopping and self.tail <= 0:
+            self.tail = None
+            self.angleDelta = 0.0
+            self.stopped = True
+
+        return buf
+
+class Poly:
+
+    def __init__(self, sample_rate=44100, block_size=441):
+        self.synths = {}
+        self.delay_buf = [0.0] * 12000
+        self.delay_position = 0
+        self.sample_rate = sample_rate
+        self.block_size = block_size
+
+    def set_sample_rate_block_size(self, rate, block_size):
+        self.sample_rate = rate
+        self.block_size = block_size
+
+    def start_note(self, note, vel):
+        if note not in self.synths:
+            self.synths[note] = Synth(sample_rate=self.sample_rate)
+            self.synths[note].start_note(note, vel)
+        elif not self.synths[note].is_active():
+            self.synths[note].start_note(note, vel)
+        else:
+            self.synths[note].renew()
+    
+    def stop_note(self, note):
+        if note in self.synths:
+            self.synths[note].stop_note()
+    
+    def render(self):
+        # This isn't a static value, it could change with each block, I'm lazily only setting it once
+        num_samples = self.block_size
+
+        if not len(self.synths):
+            cur = [0.0] * num_samples
+        else:
+            bufs = []
+            one_active = False
+            for note, synth in self.synths.items():
+                if synth.is_active():
+                    one_active = True
+                    bufs.append(synth.render(num_samples))
+            if not one_active:
+                cur = [0.0] * num_samples
+            else:
+                cur = [sum(x) for x in zip(*bufs)]
+
+        for i in range(0, num_samples):
+            in_ = cur[i]
+            cur[i] += self.delay_buf[self.delay_position]
+            self.delay_buf[self.delay_position] = (self.delay_buf[self.delay_position] + in_) * 0.5
+            self.delay_position += 1
+            if self.delay_position >= len(self.delay_buf):
+                self.delay_position = 0
+
+        return cur * 2 # mono
+
+poly = Poly()
+poly.set_sample_rate_block_size(state.sample_rate, state.block_size)
+
+def process(midi_messages, audio):
+    for m in midi_messages:
+        if m.type == "note_on":
+            if m.note < 20:
+                continue
+            poly.start_note(m.note, m.velocity/10)
+        elif m.type == "note_off":
+            poly.stop_note(m.note)
+        else:
+            print(m)
+    
+    return poly.render()
