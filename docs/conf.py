09973da679fb6ed9c7dab4c56600f7719ca7a272
tomgrek <tom.grek@gmail.com>
2024-01-20 21:39:07 -0800

    try 2

---
+++ docs/conf.py
@@ -0,0 +1,73 @@
+# This file is generated from sphinx-notes/template. DO NOT EDIT.
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+
+project = 'PyPhonic'
+author = 'Tom Grek'
+copyright = "2024, " + author
+
+# The full version, including alpha/beta/rc tags
+version = release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.githubpages',
+    'sphinxcontrib.gtagjs',
+]
+
+gtagjs_ids = ['G-E4SNX0WZYV']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The document name of the “master” document, that is,
+# the document that contains the root toctree directive.
+# Default is 'index', we set it here for supporting Sphinx<2.0
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# A boolean that decides whether codeauthor and sectionauthor directives
+# produce any output in the built files.
+show_authors = True
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo'
+
+html_theme_options = {}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_theme_options = {
+    "source_repository": "https://github.com/tomgrek/pyphonic",
+    "source_branch": "main",
+    "source_directory": "docs/",
+}
+
+# The URL which points to the root of the HTML documentation.
+# It is used to indicate the location of document like canonical_url
+html_baseurl = 'https://pyphonic.net'
+
+html_logo = html_favicon = '_images/sphinx-notes.png'
