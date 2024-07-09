52284181761bcbcbdbd496a82fdb063a0359d38b
tomgrek <tom.grek@gmail.com>
2024-07-08 20:48:51 -0700

    synth ok

--- src/pyphonic/preset_19_synth.py
+++ src/pyphonic/preset_19_synth.py
@@ -3,12 +3,15 @@
 
 import math
 import random
+from collections import defaultdict
+from copy import deepcopy
 
+import librosa
 import numpy as np
 import pyphonic
 
 from typing import Callable
-from scipy.signal import savgol_filter
+from scipy.signal import butter, iirpeak, savgol_filter, sosfilt, tf2sos
 
 
 class ADSR:
@@ -49,6 +52,54 @@ class ADSR:
                 buf.append(self.current)
         return np.array(buf, dtype=np.float32)
 
+class Filter:
+    def __init__(self, **args):
+        """
+        drywet: 1.0 is completely dry, 0.0 is completely wet
+        """
+        for k, v in args.items():
+            setattr(self, k, v)
+
+    def __call__(self, buf):
+        output, self.Z = sosfilt(self.sos, buf, zi=self.Z)
+        return buf * self.drywet + (output * (1 - self.drywet))
+    
+    def set_sample_rate_block_size(self, rate, block_size, idx):
+        self.sample_rate = rate
+        self.block_size = block_size
+        if self.type.startswith("band"):
+            self.sos = butter(self.order, [self.low, self.high], fs=self.sample_rate, btype=self.type, analog=False, output='sos')            
+            self.Z = np.zeros((self.sos.shape[0], 2))
+        elif self.type == "resonant":
+            b, a = iirpeak(self.w, self.Q, fs=self.sample_rate)
+            self.sos = tf2sos(b, a)
+            self.Z = np.zeros((self.sos.shape[0], 2))
+        else:
+            self.sos = butter(self.order, self.cutoff, fs=self.sample_rate, btype=self.type, analog=False, output='sos')
+            self.Z = np.zeros((self.sos.shape[0], 2))
+    
+    @classmethod
+    def lowpass(cls, cutoff=500, order=6, drywet=0.0):
+        return cls(cutoff=cutoff, order=order, type="lowpass", drywet=drywet)
+    
+    @classmethod
+    def highpass(cls, cutoff=500, order=6, drywet=0.0):
+        return cls(cutoff=cutoff, order=order, type="highpass", drywet=drywet)
+    
+    @classmethod
+    def bandpass(cls, low=500, high=600, order=6, drywet=0.0):
+        return cls(low=low, high=high, order=order, type="bandpass", drywet=drywet)
+    
+    @classmethod
+    def bandstop(cls, low=500, high=600, order=6, drywet=0.0):
+        return cls(low=low, high=high, order=order, type="bandstop", drywet=drywet)
+
+    @classmethod
+    def resonant(cls, w, Q, drywet=0.0):
+        return cls(w=w, Q=Q, type="resonant", drywet=drywet)
+
+wavetables = defaultdict()
+
 class Synth:
 
     @classmethod
@@ -70,16 +121,18 @@ class Synth:
         return math.sin(pos)
     
     def __init__(self, detune_coarse=0, detune=0, op="sin", gen_op=None, op_extra_params={},
-                 phase=0, rel_vel=1.0, attack=0, decay=0, sustain=0.0, release=0,
+                 phase=0, rel_vel=1.0, attack=0, decay=0, sustain=1.0, release=0,
                  delay_seconds=0.0, delay_length=0, delay_feedback=0, delay_mix=0,
                  lfo=None, lfo_freq=None, lfo_velocity=None,
                  freq_mod=None, freq_mod_freq=None, freq_mod_velocity=None,
+                 filter=None
                  ):
         self.stopped = True
 
         # Set later, in the loop
         self.sample_rate = None
         self.block_size = None
+        self.idx: int = None  # Index of the synth in the stack, to preserve data across different notes' synths
         self.delay_buf = None
 
         self.detune_coarse: int = detune_coarse
@@ -129,17 +182,24 @@ class Synth:
             assert self.freq_mod.op.startswith("gen"), "Frequency modulation is only compatible with generative operators"
             assert self.detune_coarse == 0, "LFO is not compatible with detune_coarse"
             assert self.detune == 0, "LFO is not compatible with detune"
+        
+        self.filter = filter
+        if self.filter is not None:
+            assert isinstance(self.filter, Filter), "If adding a filter, supply a Filter object"
     
-    def set_sample_rate_block_size(self, rate, block_size):
+    def set_sample_rate_block_size(self, rate, block_size, idx):
         self.sample_rate = rate
         self.block_size = block_size
+        self.idx = idx
         self.delay_buf = np.zeros(max(int(self.delay_seconds * self.sample_rate), self.block_size), dtype=np.float32)
         if self.lfo is not None:
-            self.lfo.set_sample_rate_block_size(self.sample_rate, self.block_size)
+            self.lfo.set_sample_rate_block_size(self.sample_rate, self.block_size, str(self.idx) + "_lfo")
             self.lfo.start_freq(self.lfo_freq, self.lfo_velocity)
         if self.freq_mod is not None:
-            self.freq_mod.set_sample_rate_block_size(self.sample_rate, self.block_size)
+            self.freq_mod.set_sample_rate_block_size(self.sample_rate, self.block_size, str(self.idx) + "_freq_mod")
             self.freq_mod.start_freq(self.freq_mod_freq, self.freq_mod_velocity)
+        if self.filter is not None:
+            self.filter.set_sample_rate_block_size(self.sample_rate, self.block_size, str(self.idx) + "_filter")
 
     def start_freq(self, freq, vel):
         self.level = vel
@@ -198,24 +258,49 @@ class Synth:
         elif type_ == "laser":
             ramp = np.logspace(0, 2 * np.pi, fs)
             val = np.sin(ramp * hz)
+        elif type_ == "noise":
+            val = np.random.random(fs) * 2 - 1
+        elif type_ == "wavetable": 
+            reps = int(fs // hz)
+            self.position = 0
+            
+            if self.idx in wavetables:
+                sample, orig_freq = wavetables[self.idx]
+                if orig_freq != hz:
+                    sample = librosa.resample(sample, orig_sr=self.sample_rate, target_sr=self.sample_rate * orig_freq / hz)
+            else:
+                sample = np.load(self.op_extra_params["sample"], allow_pickle=True)
+                if sample.shape[0] > 1:
+                    sample = sample[0]
+                sample /= np.max(np.abs(sample))
+                wavetables[self.idx] = (sample, self.op_extra_params.get("orig_freq", 440))
+            val = np.tile(sample, min(reps, 10))
+            self.original_wavetable_length = sample.size
+
         elif type_ == "randomwalk":
             reps = int(fs // hz)
-            randomData = bounded_random_walk(reps, lower_bound=-1, upper_bound=1, start=0, end=0, std=self.op_extra_params.get("std", 1))
-            if self.op_extra_params.get("filter_size"):
-                assert self.op_extra_params.get("filter_poly_degree"), "If you want to filter the random walk, you must supply the polynomial degree as filter_poly_degree e.g. 1, 2, 3..."
-                if randomData.size >= self.op_extra_params.get("filter_size"):
-                    filter_size = self.op_extra_params.get("filter_size")
-                else:
-                    filter_size = randomData.size
-                smooth = savgol_filter(randomData, filter_size, min(filter_size, self.op_extra_params.get("filter_poly_degree")))
+            if self.idx in wavetables:
+                smooth, orig_freq = wavetables[self.idx]
+                if orig_freq != hz:
+                    smooth = librosa.resample(smooth, orig_sr=self.sample_rate, target_sr=self.sample_rate * orig_freq / hz)
             else:
-                smooth = randomData
+                randomData = bounded_random_walk(reps, lower_bound=-1, upper_bound=1, start=0, end=0, std=self.op_extra_params.get("std", 1))
+                if self.op_extra_params.get("filter_size"):
+                    assert self.op_extra_params.get("filter_poly_degree"), "If you want to smooth the random walk, you must supply the polynomial degree as filter_poly_degree e.g. 1, 2, 3..."
+                    if randomData.size >= self.op_extra_params.get("filter_size"):
+                        filter_size = self.op_extra_params.get("filter_size")
+                    else:
+                        filter_size = randomData.size
+                    smooth = savgol_filter(randomData, filter_size, min(filter_size, self.op_extra_params.get("filter_poly_degree")))
+                else:
+                    smooth = randomData
+                wavetables[self.idx] = (smooth, hz)
             val = np.tile(smooth, min(reps, 10))
 
         # Find integer periods
         self.one_cycle = fs / hz
         
-        if type_ in ("sin", "laser"):
+        if type_ in ("sin", "laser", "noise"):
             if int(hz) != 0:
                 max_size = int(int(hz) * self.one_cycle) # working one... at least without freq shift
                 val = val[:max_size]
@@ -235,6 +320,7 @@ class Synth:
         self.envelope.start_release()
         min_delay = self.delay_buf.size // self.block_size
         self._delay_ends_in = self.delay_length * min_delay
+        self.position = 0
     
     def is_active(self):
         if self.stopped:
@@ -268,12 +354,21 @@ class Synth:
             else:
                 buf = self.waveform[:self.block_size] * self.level
                 self.waveform = np.roll(self.waveform, -self.block_size)
+                self.position += self.block_size
+                
+                if self.op == "wavetable" and self.op_extra_params.get("one_shot") and self.position > self.original_wavetable_length:
+                    self.stop_note()
+
             buf *= self.envelope.render(self.block_size)
             if self.lfo is not None:
                 lfo = self.lfo.render()
-                buf *= 1 + lfo
+                lfo = 1 + lfo
+                buf *= lfo
         else:
             buf = np.zeros(self.block_size, dtype=np.float32)
+
+        if self.filter is not None:
+            buf = self.filter(buf)
 
         if not self.no_delay:
             if self._delay_ends_in is None or self._delay_ends_in > 0:
@@ -289,64 +384,161 @@ class Synth:
 
 class Poly:
 
-    def __init__(self, sample_rate=44100, block_size=448):
+    def __init__(self, stack=[],
+                 filters=[], lfos=[],
+                 delay_seconds=0.0, delay_length=0, delay_feedback=0,
+                 sample_rate=44100, block_size=448):
         self.synths = {}
-        self.delay_buf = [0.0] * 12000
-        self.delay_position = 0
         self.sample_rate = sample_rate
         self.block_size = block_size
+
+        self.synth_stack = stack
+        self.filters = filters
+        self.lfos = lfos
+
+        self.delay_seconds = delay_seconds
+        self.delay_feedback = delay_feedback
+        self.delay_length = delay_length  # If there is delay, we do a min of 1 iteration of that delay (e.g. 1sec if delay_seconds==1) OR this many. AKA, how many echos
 
     def set_sample_rate_block_size(self, rate, block_size):
         self.sample_rate = rate
         self.block_size = block_size
+        self.delay_buf = np.zeros(max(int(self.delay_seconds * self.sample_rate), self.block_size), dtype=np.float32)
 
     def start_note(self, note, vel):
         if note not in self.synths:
-            self.synths[note] = {"synths": [
+            self.synths[note] = [deepcopy(synth) for synth in self.synth_stack]
+
+                #Synth(op="sin", rel_vel=1.0, filter=Filter.bandpass()),
+
                 # Synth(detune_coarse=0, detune=0, op="sin", rel_vel=0.5, attack=20, decay=2000, sustain=0.4, release=20, delay_feedback=0, delay_mix=0),
                 # Synth(detune_coarse=0, detune=0, op="gen", gen_op=Synth.square, rel_vel=1.0, attack=1, decay=3, sustain=0.6, release=1.1, delay_feedback=0, delay_mix=0,
                 #       freq_mod=Synth(detune_coarse=0, detune=0, op="gen", gen_op=Synth.square, rel_vel=0.5, attack=1, decay=1, sustain=0.4, release=0.0001, delay_feedback=0, delay_mix=0), freq_mod_freq=2, freq_mod_velocity=0.00005),
-                Synth(detune_coarse=0, detune=0, op="randomwalk", rel_vel=0.3, attack=0.1, decay=0.3, phase=90, sustain=0.5, release=2, delay_seconds=0.05, delay_length=2, delay_feedback=0.0, delay_mix=0,
-                      op_extra_params={"std": 2, "filter_size": 20, "filter_poly_degree": 5}),
+                
+                # Kind of a train
+                # Synth(op="saw", rel_vel=0.2, attack=0.001, decay=0.1, sustain=0.2, release=0.1, lfo=Synth(op="square", rel_vel=1.0), lfo_freq=5, lfo_velocity=1.0),
+                # Synth(op="noise", rel_vel=0.5, attack=0.3, decay=0.5, sustain=0.5, release=0.3, lfo=Synth(op="square", rel_vel=1.0), lfo_freq=5, lfo_velocity=0.8),
+                # Synth(op="gen", gen_op=Synth.noise, rel_vel=0.05, attack=0.01, decay=0.2, phase=45, sustain=0.3, release=0.1),
+
+                
+                
                 # Synth(detune_coarse=0, detune=0, op="sin", rel_vel=0.5, attack=20, decay=2000, sustain=0.4, release=20, delay_seconds=0.5, delay_length=5, delay_feedback=0.5, delay_mix=0,
-                #       lfo=Synth(sample_rate=self.sample_rate, block_size=self.block_size, op="tri", rel_vel=0.9, attack=20, decay=2000, sustain=1.0, release=20, delay_seconds=0.2, phase=90, delay_length=2, delay_feedback=0.3, delay_mix=0.2), lfo_freq=13, lfo_velocity=1.0,
+                #       lfo=Synth(op="tri", rel_vel=0.9, attack=20, decay=2000, sustain=1.0, release=20, delay_seconds=0.2, phase=90, delay_length=2, delay_feedback=0.3, delay_mix=0.2), lfo_freq=13, lfo_velocity=1.0,
                 #       ),
                 # Synth(detune_coarse=-24, op="sin", rel_vel=0.5, attack=20, decay=2000, sustain=0.7, release=20),
                 # Synth(detune_coarse=-12, op="saw", rel_vel=0.5, attack=200, decay=200, sustain=0.5, release=20000),
                 # Synth(detune_coarse=0, detune=-1, op="saw", phase=-10, attack=200, decay=200, sustain=0.5, release=100),
                 # Synth(detune=0, op="saw", attack=200, decay=200, sustain=0.5, release=2000),
                 # Synth(detune_coarse=0, detune=1, op="saw", phase=10, attack=200, decay=200, sustain=0.5, release=10000)
-            ]}
-            for synth in self.synths[note]["synths"]:
-                synth.set_sample_rate_block_size(self.sample_rate, self.block_size)
+            for i, synth in enumerate(self.synths[note]):
+                synth.set_sample_rate_block_size(self.sample_rate, self.block_size, i)
                 synth.start_note(note, vel)
         else:
-            for synth in self.synths[note]["synths"]:
+            for synth in self.synths[note]:
                 synth.renew()
     
     def stop_note(self, note):
         if note in self.synths:
-            for synth in self.synths[note]["synths"]:
+            for synth in self.synths[note]:
                 synth.stop_note()
     
     def render(self):
         buf = np.zeros(self.block_size, dtype=np.float32)
         for _, synths in self.synths.items():
-            for synth in synths["synths"]:
+            for synth in synths:
                 this = synth.render()
                 if this is not None:
                     buf += this
+        
+        for lfo, _, _ in self.lfos:
+            lfo = lfo.render()
+            lfo = 1 + lfo
+            buf *= lfo
+        
+        for filter in self.filters:
+            buf = filter(buf)
+        
+        if self.delay_feedback > 0:
+            self.delay_buf = np.roll(self.delay_buf, -self.block_size)
+            buf += self.delay_buf[:self.block_size] * self.delay_feedback
+            self.delay_buf[-self.block_size:] = buf
+
         return buf
 
-poly = Poly()
+presets = {
+    "basic": {
+        "stack": [(Synth, {"op": "sin", "rel_vel": 1.0})],
+        "filters": [(Filter.resonant, {"w": 200, "Q": 50, "drywet": 0.5})],
+        "lfos": [ 
+            {"synth": (Synth, {"op": "sin", "rel_vel": 1.0}), "freq": 4, "vel": 0.2},
+            {"synth": (Synth, {"op": "tri", "rel_vel": 1.0}), "freq": 10, "vel": 0.4},
+        ]
+    },
+    "cello": {
+        "stack": [
+            (Synth, {"detune_coarse": 0, "detune": 0, "op": "randomwalk", "rel_vel": 0.9, "attack": 0.1, "decay": 0.3, "phase": 90, "sustain": 0.5, "release": 2, "delay_seconds": 0.05, "delay_length": 2, "delay_feedback": 0.0, "delay_mix": 0,
+                    "op_extra_params": {"std": 2, "filter_size": 20, "filter_poly_degree": 5}}),
+            (Synth, {"detune_coarse": 0, "detune": -10, "op": "randomwalk", "rel_vel": 0.6, "attack": 0.05, "decay": 0.1, "phase": 0, "sustain": 0.2, "release": 0.01, "delay_seconds": 0.05, "delay_length": 2, "delay_feedback": 0.0, "delay_mix": 0,
+                    "op_extra_params": {"std": 0.1}}),
+            (Synth, {"detune_coarse": 0, "detune": 11, "op": "randomwalk", "rel_vel": 0.3, "attack": 0.01, "decay": 0.2, "phase": 45, "sustain": 0.3, "release": 0.1, "delay_seconds": 0.05, "delay_length": 2, "delay_feedback": 0.0, "delay_mix": 0,
+                    "op_extra_params": {"std": 0.2}})
+        ],
+        "filters": [(Filter.lowpass, {"cutoff": 4000, "order": 4, "drywet": 0.5})], "lfos": [{"synth": (Synth, {"op": "sin", "rel_vel": 1.0}), "freq": 10, "vel": 0.1}],
+        "delay_seconds": 0.15, "delay_length": 2, "delay_feedback": 0.12
+    }
+}
+
+def get_preset(name):
+    preset = presets[name]
+    synth_stack = []
+    for stack_item in preset["stack"]:
+        cls, params = stack_item
+        synth_stack.append(cls(**params))
+    filter_stack = []
+    for filter in preset["filters"]:
+        cls, params = filter
+        filter_stack.append(cls(**params))
+    lfo_stack = []
+    for lfo in preset["lfos"]:
+        synth, params = lfo["synth"]
+        lfo_stack.append((synth(**params), lfo["freq"], lfo["vel"]))
+    preset.pop("stack")
+    preset.pop("filters")
+    preset.pop("lfos")
+    return {
+        "stack": synth_stack,
+        "filters": filter_stack,
+        "lfos": lfo_stack,
+        **preset
+    }
+
+# poly = Poly(**get_preset("cello"))
+poly = Poly(stack=[Synth(op="wavetable", rel_vel=1.0, op_extra_params={"one_shot": True, "orig_freq": 5000, "sample": pyphonic.getDataDir() + "/chh.pkl"})])
+
+# Or, to define one from scratch:
+# poly = Poly(
+#     stack=[Synth(op="sin", rel_vel=1.0)],
+#     filters=[Filter.resonant(w=200, Q=50, drywet=0.5)],
+#     lfos=[(Synth(op="sin", rel_vel=1.0), 4, 0.1),
+#          (Synth(op="tri", rel_vel=1.0), 10, 0.4)],
+# )
+
 started = None
 
 def process_npy(midi_messages, audio):
     global started
     num_samples = len(audio[0])
+
     if started is None:
         poly.set_sample_rate_block_size(pyphonic.getSampleRate(), num_samples)
         started = True
+        for i, filter in enumerate(poly.filters):
+            filter.set_sample_rate_block_size(pyphonic.getSampleRate(), num_samples, i)
+        for i, lfo in enumerate(poly.lfos):
+            lfo_synth, freq, vel = lfo
+            lfo_synth.set_sample_rate_block_size(pyphonic.getSampleRate(), num_samples, i)
+            lfo_synth.start_freq(freq, vel)
+
     for m in midi_messages:
         if m.type == "note_on":
             if m.note < 20:
